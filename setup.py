fc66cbc8be5b32aed686c10d94047f57525b2aff
eelco <eelcovv@gmail.com>
2022-07-28 13:33:28 +0200

    update to pyscaffold 4

--- setup.py
+++ setup.py
@@ -1,25 +1,21 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 """
-    Setup file for latlon.
+    Setup file for LatLon3.
+    Use setup.cfg to configure your project.
 
-    This file was generated with PyScaffold 2.5.6, a tool that easily
-    puts up a scaffold for your new Python project. Learn more under:
-    http://pyscaffold.readthedocs.org/
+    This file was generated with PyScaffold 4.2.1.
+    PyScaffold helps you to put up the scaffold of your new Python project.
+    Learn more under: https://pyscaffold.org/
 """
-
-import sys
-
 from setuptools import setup
 
-
-def setup_package():
-    needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
-    sphinx = ['sphinx'] if needs_sphinx else []
-    setup(setup_requires=['six', 'pyscaffold>=2.5a0,<2.6a0'] + sphinx,
-          long_description_content_type="text/markdown",
-          use_pyscaffold=True)
-
-
 if __name__ == "__main__":
-    setup_package()
+    try:
+        setup(use_scm_version={"version_scheme": "no-guess-dev"})
+    except:  # noqa
+        print(
+            "\n\nAn error occurred while building the project, "
+            "please ensure you have the most updated version of setuptools, "
+            "setuptools_scm and wheel with:\n"
+            "   pip install -U setuptools setuptools_scm wheel\n\n"
+        )
+        raise
