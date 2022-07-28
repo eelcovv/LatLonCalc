fb1bd36a46d7eb05c271ad0270a024ecfcbdb6e0
eelco <eelcovv@gmail.com>
2022-07-28 15:03:00 +0200

    update test to python 3.9

--- tests/test_LatLon.py
+++ tests/test_LatLon.py
@@ -1,11 +1,13 @@
-'''
+"""
 Test routines for class LatLon3 in package LatLon3
 Designed for use with pytest
 
 Created on Sep 2, 2014
 
 @author: gdelraye
-'''
+
+update to Python 3.9: E. van Vliet (July, 2022)
+"""
 
 import pyproj
 from numpy import (exp, angle, deg2rad, rad2deg)
@@ -38,23 +40,22 @@ def test_latlon_fromstring():
     lat_str, lon_str = '5.8833', '-162.0833'  # test location is Palmyra Atoll
     # Convert decimal degrees string to LatLon3 object:
     palmyra = string2latlon(lat_str, lon_str, 'D')
-    assert str(
-        palmyra) == '5.8833, -162.0833'  # Failure to convert from degree, minutes, second, hemisphere string
+    # Failure to convert from degree, minutes, second, hemisphere string
+    assert str(palmyra) == '5.8833, -162.0833'
     lat_str, lon_str = '5, 52, 59.88, N', '162, 4, 59.88, W'
     # Convert degrees minutes second string with hemisphere identifier to LatLon3 object:
     palmyra = string2latlon(lat_str, lon_str, 'd%, %m%, %S%, %H')
     assert_almost_equal(palmyra.lat.decimal_degree, 5.8833)
-    assert_almost_equal(palmyra.lon.decimal_degree,
-                        -162.0833)  # Failure to convert from degree, minutes, second, hemisphere string
+    # Failure to convert from degree, minutes, second, hemisphere string
+    assert_almost_equal(palmyra.lon.decimal_degree, -162.0833)
     lat_str, lon_str = 'N_5deg 52.998', 'W_162deg 4.998'
     # Convert degrees minutes second string with fancy separators to LatLon3 object:
     palmyra = string2latlon(lat_str, lon_str, 'H%_%d%deg %M')
     # convert to lat decimal degree to solve round of problem
-    assert_almost_equal(palmyra.lat.decimal_degree,
-                        Latitude(
-                            5.8833).decimal_degree)  # Failure to convert from hemisphere, degree, minutes string
-    assert_almost_equal(palmyra.lon.decimal_degree, Latitude(
-        -162.0833).decimal_degree)  # Failure to convert from hemisphere, degree, minutes string
+    # Failure to convert from hemisphere, degree, minutes string
+    assert_almost_equal(palmyra.lat.decimal_degree, Latitude(5.8833).decimal_degree)
+    # Failure to convert from hemisphere, degree, minutes string
+    assert_almost_equal(palmyra.lon.decimal_degree, Latitude(-162.0833).decimal_degree)
 
 
 def test_latlon_complex():
@@ -71,8 +72,9 @@ def test_latlon_heading():
     """
     Test LatLon3 methods heading_initial and heading_reverse
     """
+    # locations: Palmyra Atoll and Honolulu, HI
     palmyra, honolulu = LatLon(5.8833, -162.0833), LatLon(21.3,
-                                                          -157.8167)  # locations: Palmyra Atoll and Honolulu, HI
+                                                          -157.8167)
     true_heading = 14.691  # Correct heading in from Palmyra to Honolulu to 3 decimal places
     forward_heading = palmyra.heading_initial(honolulu)  # Initial heading from palmyra to honolulu
     # Check heading from Palmyra Atoll to Honolulu using heading_initial:
@@ -80,7 +82,8 @@ def test_latlon_heading():
     reverse_heading = honolulu.heading_reverse(palmyra)  # Reverse heading from honolulu to palmyra
     # Check heading from Palmyra Atoll to Honolulu using heading_reverse:
     assert_almost_equal(reverse_heading, true_heading, decimal=3)
-    # Now for two locations with (for our purposes) the same longitude - Washington, DC and Lima, Peru:
+    # Now for two locations with (for our purposes) the same longitude - Washington, DC and
+    # Lima, Peru:
     washington, lima = LatLon(Latitude(38, 54), Longitude(-77, -2)), LatLon(Latitude(-12, -3),
                                                                             Longitude(-77, -2))
     true_heading = -180.000  # Heading for directly south
@@ -89,13 +92,15 @@ def test_latlon_heading():
     try:
         assert_almost_equal(forward_heading, true_heading, decimal=3)
     except AssertionError:
-        # now exactly south is 180 (not -180). Just a definition matter. Try again with 180 if -180 fails
+        # now exactly south is 180 (not -180). Just a definition matter. Try again with 180 if
+        # -180 fails
         assert_almost_equal(forward_heading, -true_heading, decimal=3)
     reverse_heading = lima.heading_reverse(washington)
     # Check handling of equal longitude coordinates by heading_reverse:
     true_heading = 180.000  # Heading for directly south
     assert_almost_equal(reverse_heading, true_heading, decimal=3)
-    # Now for two locations with (for our purposes) the same latitude - Alexandria, Egypt and Shanghai, China:
+    # Now for two locations with (for our purposes) the same latitude - Alexandria, Egypt and
+    # Shanghai, China:
     alexandria, shanghai = LatLon(Latitude(31, 12), Longitude(29, 55)), LatLon(Latitude(31, 12),
                                                                                Longitude(121, 30))
     true_heading = 61.941  # Correct heading in from Alexandria to Shanghai to 3 decimal places
@@ -110,38 +115,39 @@ def test_latlon_heading():
 
 
 def test_latlon_distance():
-    '''
+    """
     Test LatLon3 method distance
-    '''
-    palmyra, honolulu = LatLon(5.8833, -162.0833), LatLon(21.3,
-                                                          -157.8167)  # locations: Palmyra Atoll and Honolulu, HI
+    """
+    # locations: Palmyra Atoll and Honolulu, HI
+    palmyra, honolulu = LatLon(5.8833, -162.0833), LatLon(21.3, -157.8167)
     true_distance = '1766.691'  # Distance from Palmyra to Honolulu in km
     wgs84_distance = palmyra.distance(honolulu)
-    assert '%.3f' % (
-        wgs84_distance) == true_distance  # Failed to calculate WGS84 distance from Palmyra to Honolulu
+    # Failed to calculate WGS84 distance from Palmyra to Honolulu
+    assert '{:.3f}'.format(wgs84_distance) == true_distance
     wgs84_distance = honolulu.distance(palmyra)
-    assert '%.3f' % (
-        wgs84_distance) == true_distance  # Failed to calculate WGS84 distance from Honolulu to Palmyra
+    # Failed to calculate WGS84 distance from Honolulu to Palmyra
+    assert '{:.3f}'.format(wgs84_distance) == true_distance
     # Now including the north pole:
     geographic_north = LatLon(90, 0)  # Longitude is meaningless in this case
     true_distance = '7645.673'  # Distance from Honolulu to North Pole in km
     wgs84_distance = honolulu.distance(geographic_north)
-    assert '%.3f' % (
-        wgs84_distance) == true_distance  # Failed to calculate WGS84 distance from north pole
+    # Failed to calculate WGS84 distance from north pole
+    assert '{:.3f}'.format(wgs84_distance) == true_distance
 
 
 def test_latlon_offset():
-    '''
+    """
     Test LatLon3 method offset
-    '''
-    palmyra, honolulu = LatLon(5.8833, -162.0833), LatLon(21.3,
-                                                          -157.8167)  # locations: Palmyra Atoll and Honolulu, HI
+    """
+    # locations: Palmyra Atoll and Honolulu, HI
+    palmyra, honolulu = LatLon(5.8833, -162.0833), LatLon(21.3, -157.8167)
     distance = palmyra.distance(honolulu)  # WGS84 distance is 1766.69130376 km
     initial_heading = palmyra.heading_initial(
         honolulu)  # Initial heading to Honolulu on WGS84 ellipsoid
-    offset_hnl = palmyra.offset(initial_heading,
-                                distance)  # Reconstruct lat/lon for Honolulu based on offset from Palmyra
-    # Equality could also be tested with honolulu == offset_hnl, but would be subject to float errors
+    offset_hnl = palmyra.offset(initial_heading, distance)
+    # Reconstruct lat/lon for Honolulu based on offset from Palmyra
+    # Equality could also be tested with honolulu == offset_hnl, but would be subject to float
+    # errors
     assert honolulu.almost_equal(offset_hnl)
     vector = honolulu - palmyra  # Same process with GeoVectors
     vector_hnl = palmyra + vector  # Reconstruct lat/lon for Honolulu based on offset from Palmyra
@@ -149,9 +155,9 @@ def test_latlon_offset():
 
 
 def test_latlon_project():
-    '''
+    """
     Test LatLon3 method project
-    '''
+    """
     palmyra = LatLon(5.8833, -162.0833)  # test location is Palmyra Atoll
     projection = pyproj.Proj(proj='utm', zone=3, ellps='WGS84')
     x, y = palmyra.project(projection)
--- tests/test_geovector.py
+++ tests/test_geovector.py
@@ -5,9 +5,10 @@ Designed for use with pytest
 Created on Sep 2, 2014
 
 @author: gdelraye
+
+update to Python 3.9: E. van Vliet (July, 2022)
 """
 
-import pytest
 import math
 
 from LatLon3.LatLon import GeoVector
@@ -22,7 +23,8 @@ def test_constructor():
     initial_heading = 45.  # Compass heading
     vector1 = GeoVector(dx=dx, dy=dy)
     vector2 = GeoVector(initial_heading=initial_heading, distance=magnitude)
-    assert vector1.almost_equals(vector2)  # Error in Updating GeoVector angle and magnitude from dx and dy
+    # Error in Updating GeoVector angle and magnitude from dx and dy
+    assert vector1.almost_equals(vector2)
 
 
 def test_magic():
@@ -33,15 +35,22 @@ def test_magic():
     vector2 = GeoVector(dx=1, dy=0)
     assert -vector1.dy == -1  # Unexpected behavior in __neg__ method
     vector1add2 = vector1 + vector2
-    assert vector1add2.dx == 1 and vector1add2.dy == 1  # Failed to construct correct vector with __add__ method
-    assert abs(vector1add2.magnitude - math.sqrt(2)) < 0.00001  # Failed to get correct magnitude with __add__ method
+    # Failed to construct correct vector with __add__ method
+    assert vector1add2.dx == 1 and vector1add2.dy == 1
+    # Failed to get correct magnitude with __add__ method
+    assert abs(vector1add2.magnitude - math.sqrt(2)) < 0.00001
     vector2sub1 = vector2 - vector1
-    assert vector2sub1.dx == 1 and vector2sub1.dy == -1  # Failed to construct correct vector using __sub__ method
-    assert vector2sub1.magnitude == vector1add2.magnitude  # Failed to properly compute magnitude with __add__ method
-    assert vector2sub1.heading - 90 == vector1add2.heading  # Failed to properly compute heading with __sub__ method
+    # Failed to construct correct vector using __sub__ method
+    assert vector2sub1.dx == 1 and vector2sub1.dy == -1
+    # Failed to properly compute magnitude with __add__ method
+    assert vector2sub1.magnitude == vector1add2.magnitude
+    # Failed to properly compute heading with __sub__ method
+    assert vector2sub1.heading - 90 == vector1add2.heading
     vector1x2 = vector1 * 2
-    assert vector1.heading == vector1x2.heading  # Unexpected behavior for heading attribute with __mul__ method
+    # Unexpected behavior for heading attribute with __mul__ method
+    assert vector1.heading == vector1x2.heading
     assert vector1x2 > vector1  # __cmp__ method giving unexpected result
     assert vector1 < vector1x2  # __cmp__ method giving unexpected result
-    assert vector1x2.almost_equals(vector1 + vector1)  # Failed to recreate vector by addition and multiplication
+    # Failed to recreate vector by addition and multiplication
+    assert vector1x2.almost_equals(vector1 + vector1)
     assert str(vector1x2) == '0.0, 2'  # String conversion failed
